e224c7550a214a31c9c5f40d8da434033085037a
BuchtaMakova <dajula44@gmail.com>
2023-04-13 14:17:50 +0200

    oof

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import {FormsModule} from "@angular/forms";
+import {CategoryComponent} from "./components/category/category.component";
+import {HttpClientModule} from "@angular/common/http";
+import { MaterialModule } from './mat-module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    NavbarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+      HttpClientModule,
+      MaterialModule,
+      BrowserAnimationsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit,ViewChild} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {MatAccordion} from '@angular/material/expansion';
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit{
+  @ViewChild(MatAccordion) accordion: MatAccordion;
+  categories: any [] = [];
+
+  visible = false;
+
+  toggleCollapse(): void {
+    this.visible = !this.visible;
+  }
+constructor(private http:HttpClient) {
+  this.accordion = new MatAccordion();
+}
+  ngOnInit(): void {
+this.fetchCategories();
+  }
+fetchCategories(){
+  const url = 'https://localhost:7100/api/Categories/GetCategories';
+  let header = {
+    headers: new HttpHeaders().set(
+      'Authorization',
+      'Bearer ' + ""
+    ),
+  };
+  this.http.get<any>(url, header).subscribe(
+    (response: any) => {
+      this.categories = response;
+    },
+    (error: any) => {
+      if (error.status == 401) {
+        console.log(error);
+      }
+    }
+  );
+}
+}
